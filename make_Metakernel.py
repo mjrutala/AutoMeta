6f20ba601d9d7e8871e6dce39b3cc96a12ea1ddf
mjrutala <mrutala@bu.edu>
2024-03-11 14:30:10 +0000

    Commented out use of urllib temporarily

--- make_Metakernel.py
+++ make_Metakernel.py
@@ -206,27 +206,30 @@ def run_wgetForSPICE(url, savedir, namepattern, show_progress=True, force_update
     
     subprocess.run(commandline)
 
-def run_urllibForSPICE(url, savedir, namepattern, show_progress=True, force_update=False):
+# def run_urllibForSPICE(url, savedir, namepattern, show_progress=True, force_update=False):
     
-    #  In case savedir is being handled as a pathlib Path, which subprocess
-    #  doesn't like
-    savedir = str(savedir)
+#     #  In case savedir is being handled as a pathlib Path, which subprocess
+#     #  doesn't like
+#     savedir = str(savedir)
     
-    html_url = url
-    with urllib.request.urlopen(url) as response:
-        html_body = response.read().decode('utf-8')
-    soup = BeautifulSoup(html_body, 'html.parser')
+#     html_url = url
+#     with urllib.request.urlopen(url) as response:
+#         html_body = response.read().decode('utf-8')
+#     soup = BeautifulSoup(html_body, 'html.parser')
     
-    href_link_list = list()
-    for a in soup.find_all('a', href=True):
-        href_link_list.append(a['href'])
+#     href_link_list = list()
+#     for a in soup.find_all('a', href=True):
+#         href_link_list.append(a['href'])
     
-    #  Check if namepattern occurs in href_link_list
+#     #  Check if namepattern occurs in href_link_list
     
-    #  Construct new urls including above matches
+#     #  Construct new urls including above matches
     
-    #  url request complete url names
+#     #  url request complete url names
     
+    
+#     with file as urllib.request.URLopener():
+#         file.retreive(url)
     
 def make_Metakernel(spacecraft, basedir = '', force_update=False):
     
